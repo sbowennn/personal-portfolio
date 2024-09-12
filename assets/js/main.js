fae0e00d24b864594af40daff504af4635fc9247
Sasquia Bowen <sbowennn@hotmail.com>
2024-09-12 10:30:39 +0100

    added html new icon, css and js to show and hide menu

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+/*--SHOW/HIDE MENU--*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*--SHOW--*/
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
+
+/*--HIDE--*/
+if(navClose){
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+/*--REMOVE MOBILE MENU--*/
+const navLink = document.querySelectorAll('nav__link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*--SKILLS--*/
+
+/*--TABS --*/
+
+/*--SERVICES--*/
+
+
+/*----*/
+
+/*----*/
+
+
+/*----*/
+
+
+/*----*/
+
+
+/*----*/
+
+/*----*/
+
+/*----*/
