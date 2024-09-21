d1f5ad7df41766dbed039399979f2498cdce6109
Sasquia Bowen <sbowennn@hotmail.com>
2024-09-21 12:10:54 +0100

    updated js file

--- files/js/main.js
+++ files/js/main.js
@@ -28,6 +28,24 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*--SKILLS--*/
 
+const skillsContent = document.getElementsByClassName('skills-content'),
+      skillsHeader = document.querySelectorAll('skills-header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.getElementsByClassName
+
+    for(i = 0; i < skillsContent.length; i++){
+      skillsContent[i].className = 'skills-content skills-close'
+    }
+    if(itemClass === 'skills-content skills-close'){
+      this.parentNode.className = 'skills-content skills-open'
+    }
+} 
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills)
+})
+
 /*--TABS --*/
 
 /*--SERVICES--*/
