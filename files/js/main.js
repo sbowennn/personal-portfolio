65c94f5fdadefd5cebaeed66563d67c1ee74e91d
Sasquia Bowen <sbowennn@hotmail.com>
2024-09-27 14:40:00 +0100

    changing js menu

--- files/js/main.js
+++ files/js/main.js
@@ -13,30 +13,30 @@ document.getElementById('nav-menu').addEventListener('click', function() {
 /*--SHOW/HIDE MENU
 const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close'); --*/
+      navClose = document.getElementById('nav-close');
 
-/*--SHOW--*/
+SHOW
 if(navToggle){
   navToggle.addEventListener('click', () => {
     navMenu.classList.add('show-menu');
   });
 }
 
-/*--HIDE--*/
+HIDE
 if(navClose){
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu');
   });
 }
 
-/*--REMOVE MOBILE MENU--*/
+REMOVE MOBILE MENU
 const navLink = document.querySelectorAll('.nav-link')
 
 function linkAction(){
   const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction)); --*/
 
 /*--SKILLS--*/
 
