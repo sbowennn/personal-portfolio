2c85a6bd7f283976fc3b7feca86ebe29c5a59a62
Sasquia Bowen <sbowennn@hotmail.com>
2024-09-28 11:39:47 +0100

    changed js

--- files/js/main.js
+++ files/js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('nav-menu').addEventListener('click', function() {
+document.getElementById('menu').addEventListener('click', function() {
   const menu = document.getElementById('menu');
   if (menu.style.display === 'none' || menu.style.display === '') {
       menu.style.display = 'block';  // Show menu
