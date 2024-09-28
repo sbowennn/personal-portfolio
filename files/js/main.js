8ca2ecfb9008940dbf57c8fdaca128a216a030d4
Sasquia Bowen <sbowennn@hotmail.com>
2024-09-28 11:32:12 +0100

    added details and try to fixed bugs

--- files/js/main.js
+++ files/js/main.js
@@ -141,6 +141,12 @@ window.addEventListener('scroll', scrollActive)
 
 /*--BACKGROUND HEADER--*/
 
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 
 /*----*/
 
